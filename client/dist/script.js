231c26cdba6fb9ce3a0318693b770599317a54a1
Kristin Faner <seeksort@gmail.com>
2019-10-01 22:48:57 -0500

    add html form and handling

--- client/dist/script.js
+++ client/dist/script.js
@@ -1,10 +1,14 @@
 "use strict";
 
-function createTalk() {
+function createTalk(event) {
+    event.preventDefault()
+    const title = document.forms.talk_info.elements[0].value
+    const twitter_handle = document.forms.talk_info.elements[1].value
     const xmlHttp = new XMLHttpRequest()
     xmlHttp.open("POST", "/dev/create_talk", false)
     const body = {
-        "title": "test title"
+        "title": title,
+        "twitter_handle": twitter_handle
     }
     xmlHttp.send(JSON.stringify(body));
     document.getElementById("message").innerHTML = xmlHttp.responseText
