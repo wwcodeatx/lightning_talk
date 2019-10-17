b92ece69baa3410ddc56590b2ccbee8ec832707a
ceci21 <ceci@cpe-172-100-0-217.twcny.res.rr.com>
2019-10-16 21:07:28 -0500

    syntax formatting

--- client/dist/script.js
+++ client/dist/script.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
 function createTalk(event) {
-    event.preventDefault()
-    const { elements: fields } = document.forms.talk_info
-    const title = fields[0].value
-    const speaker_name = fields[1].value
-    const twitter_handle = fields[2].value
-    const xmlHttp = new XMLHttpRequest()
-    xmlHttp.open("POST", "/dev/create_talk", false)
-    const body = {
-        "title": title,
-        "speaker_name": speaker_name,
-        "twitter_handle": twitter_handle
-    }
-    xmlHttp.send(JSON.stringify(body));
-    document.getElementById("message").innerHTML = xmlHttp.responseText
+  event.preventDefault();
+  const { elements: fields } = document.forms.talk_info;
+  const title = fields[0].value;
+  const speaker_name = fields[1].value;
+  const twitter_handle = fields[2].value;
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.open('POST', '/dev/create_talk', false);
+  const body = {
+    title: title,
+    speaker_name: speaker_name,
+    twitter_handle: twitter_handle
+  };
+  xmlHttp.send(JSON.stringify(body));
+  document.getElementById('message').innerHTML = xmlHttp.responseText;
 }
