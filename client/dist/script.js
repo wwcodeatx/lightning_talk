2dd92d91c248833501210011f21d24c2657c4053
ceci21 <ceci@cpe-172-100-0-217.twcny.res.rr.com>
2019-10-18 18:46:44 -0500

    signup -> sign-up, code spacing

--- client/dist/script.js
+++ client/dist/script.js
@@ -8,11 +8,31 @@ function createTalk(event) {
   const twitter_handle = fields[2].value;
   const xmlHttp = new XMLHttpRequest();
   xmlHttp.open('POST', '/dev/create_talk', false);
+
+  xmlHttp.addEventListener('load', function() {
+    const messageEl = document.getElementById('message');
+    const optionalName = speaker_name || twitter_handle || '';
+    const optionalTitle = title || '';
+    const successMessage = '' +
+      'Thanks' +
+      ((optionalName) ? ', ' + optionalName : '') + '! ' +
+      'Your talk ' +
+      ((optionalTitle) ? `"${optionalTitle}"` : '') + ' ' +
+      'is successfully submitted!';
+    messageEl.innerHTML = successMessage;
+  });
+
+  xmlHttp.addEventListener('error', function() {
+    const messageEl = document.getElementById('message');
+    const errorMessage = 'ERROR: There was a problem submitting your talk.'
+    messageEl.innerHTML = errorMessage;
+  });
+
   const body = {
     title: title,
     speaker_name: speaker_name,
     twitter_handle: twitter_handle
   };
+
   xmlHttp.send(JSON.stringify(body));
-  document.getElementById('message').innerHTML = xmlHttp.responseText;
 }
