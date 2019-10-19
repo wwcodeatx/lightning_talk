7e2988245572b16f4873ad9651f1f02e91e927ca
ceci21 <ceci@cpe-172-100-0-217.twcny.res.rr.com>
2019-10-19 16:57:49 -0500

    add moment.js lib, populate date dropdown

--- client/dist/script.js
+++ client/dist/script.js
@@ -20,23 +20,50 @@ function createTalk(event) {
 
 function getFirstMondays(maxMondaysRetrieved = 5) {
   // Gets first n (maxMondaysRetrieved) Mondays from today's date
+  const possibleFirstMondays = [];
+
   let today = new Date();
   let month = today.getMonth(); // Months are 0-indexed
   let year = today.getFullYear();
+  let d = 1;
 
-  const possibleMondays = [];
-
-  for (let d = 1; d <= 7 && possibleMondays.length < maxMondaysRetrieved; d++) {
+  while (d <= 7 && possibleFirstMondays.length < maxMondaysRetrieved) {
     let date = new Date(year, month, d);
     if (date.getDay() === 1) {
       // Since days of the week are also 0-indexed, it's a Monday!
-      if (month === today.getMonth() && today.getDate() > d) {
-      } else {
-        possibleMondays.push(date);
+      if (!(month === today.getMonth() && today.getDate() > d)) {
+        // If today's date is ahead of the first monday that we're looking at, we should exclude it
+        possibleFirstMondays.push(date);
       }
-      month = (month + 1 < 12) ? month + 1 : ++year && 0;
+      if (month + 1 < 12) {
+        month = month + 1;
+      } else {
+        year += 1;
+        month = 0;
+      }
+    }
+
+    if (d + 1 > 7) {
+      // Reset day-of-week counter when necessary
       d = 1;
+    } else {
+      d++
     }
   }
-  return possibleMondays;
+  return possibleFirstMondays;
 }
+
+function populateDateDropdown() {
+  const mondays = getFirstMondays();
+  const dropdownEl = document.querySelector('select[name="talk_date"]');
+
+  mondays.forEach(function(date, i) {
+    const optionEl = document.createElement('option');
+    const dateString = moment(date.getTime()).format('dddd MMMM Do, YYYY');
+    optionEl.value = moment(date.getTime()).format('YYYY-MM-DD');
+    optionEl.innerHTML = dateString;
+    dropdownEl.appendChild(optionEl);
+  });
+}
+
+populateDateDropdown();
