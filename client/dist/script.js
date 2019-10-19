ec0d82c96f38c482225db2aac84d8114cb36ef5f
ceci21 <ceci@ODYSSEUS.local>
2019-10-18 23:51:56 -0500

    check form on input change, style *'s, disable submit if invalid

--- client/dist/script.js
+++ client/dist/script.js
@@ -1,7 +1,37 @@
 'use strict';
 
+function isFormValid() {
+  const { elements: fields } = document.forms.talk_info;
+  const title = fields[0].value;
+  const speaker_name = fields[1].value;
+  if (!title || !speaker_name) {
+    return false;
+  }
+
+  if (title.length > 0 && speaker_name.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function enableSubmit(isBtnEnabled) {
+  const submitBtn = document.querySelector('button[type="submit"]');
+  if (submitBtn.disabled === isBtnEnabled) {
+    submitBtn.disabled = !isBtnEnabled;
+  }
+}
+
+function inputChangeHandler() {
+  const formIsValid = isFormValid();
+  enableSubmit(formIsValid);
+}
+
 function createTalk(event) {
   event.preventDefault();
+  if (!isFormValid()) {
+    return;
+  }
   const { elements: fields } = document.forms.talk_info;
   const title = fields[0].value;
   const speaker_name = fields[1].value;
